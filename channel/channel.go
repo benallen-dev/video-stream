ef0618c8335b4f67a4a3d3002b589f8c72f85a07
Ben Allen <ben@benallen.dev>
2025-08-30 11:59:48 +0200

    Make streamfile a method of channel

--- channel/channel.go
+++ channel/channel.go
@@ -1,7 +1,10 @@
 package channel
 
 import (
+	"path"
+	"sync"
 	"fmt"
+	"os/exec"
 	"time"
 
 	"video-stream/log"
@@ -48,10 +51,12 @@ func (c *Channel) AddClient() (chan []byte, func()) {
 	return c.connections.add()
 }
 
-
 func (c *Channel) Start() {
+	// Hang out until AddClient is called or parent context is canceled
+	// Start streaming media until media context or parent context is canceled
+	// GOTO 10
 	for {
-		streamFile(c.schedule.randomFile(), c.connections.broadcast)
+		c.streamFile(c.schedule.randomFile())
 
 		// Space out new files a little bit so clients can catch up
 		var DELAY = 2
@@ -61,6 +66,7 @@ func (c *Channel) Start() {
 		}
 	}
 }
+
 
 // Useful for debugging but not something I actually want to expose
 
@@ -75,4 +81,105 @@ func (c *Channel) String() string {
 
 func (c *Channel) Count() int {
 	return c.connections.Count()
+}
+
+func (c *Channel) streamFile(f mediafile) {
+
+	var audioMap string	
+	if f.hasEnglishAudio() {
+		log.Debug("Mapping eng audio stream")
+		audioMap = "0:a:m:language:eng"
+	} else {
+		log.Debug("Mapping all audio streams")
+		audioMap= "0:a"
+	}
+
+	ffmpegArgs := []string{
+		// Avoid timestamp funkiness
+		"-fflags", "+genpts",
+		"-avoid_negative_ts", "make_zero",
+
+		// Get input
+		// "-sseof", "-10", // start N seconds from the end
+		// "-ss", "45", // skip the first 45 seconds
+		"-re", // throttle to realtime
+		"-i", f.path,
+
+		// Map streams
+		"-map", "0:v:0",
+		"-map", audioMap,
+
+		// Re-encode video to h.264 1920x1080
+		"-c:v", "libx264",
+		"-preset", "veryfast",
+		"-vf", "scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2", // letterbox 1080p
+
+		// Re-encode audio to 48kHz stereo AAC
+		"-c:a", "aac",
+		"-ar", "48000",
+		"-ac", "2",
+		"-b:a", "128k",
+
+		"-f", "mpegts", // format into mpegts so we can just dump it over http
+		"pipe:1", // use stdout so we can pipe it into our go program
+	}
+
+	cmd := exec.Command("ffmpeg", ffmpegArgs...)
+
+	dur, err := f.Duration()
+	if err != nil {
+		log.Warn("couldn't get file duration", "error", err.Error())
+	}
+	log.Info("Running ffmpeg", "file", path.Base(f.path), "duration", dur)
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal("could not create stdout pipe", "error", err.Error())
+	}
+
+	// TODO: Figure out how to get output from stderr for debugging without blocking
+	// stderr, err := cmd.StderrPipe()
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal("could not run ffmpeg command", "error", err.Error())
+	}
+
+	var innerWg sync.WaitGroup
+
+	// Pump ffmpeg → broadcast
+	innerWg.Go( func() {
+		buf := make([]byte, 4096)
+
+		for {
+			n, err := stdout.Read(buf)
+			if err != nil {
+				log.Info("ffmpeg ended:", "reason", err)
+				log.Debug(cmd.String())
+
+				// for {
+				// 	n, err := stderr.Read(buf)
+				// 	log.Debug("Stderr:", "contents", string(buf[:n]))
+				// 	if err != nil {
+				// 		log.Debug("Error reading stderr:", "error", err.Error())
+				// 		break
+				// 	}
+				// }
+				break
+			}
+			if n > 0 {
+				data := make([]byte, n)
+				copy(data, buf[:n])
+				c.connections.broadcast(data)
+			}
+			if n == 0 {
+				log.Warn("Read zero bytes")
+			}
+		}
+	})
+
+	innerWg.Wait()
+
 }
